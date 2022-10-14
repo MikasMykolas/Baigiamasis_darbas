527eefe2d9cad5d261115ce6ed3f54544a6bb2c9
bmyko <b.mykolas@gmail.com>
2022-10-14 09:27:21 +0300

    Set up options and driver in Common class

--- src/test/java/pages/Common.java
+++ src/test/java/pages/Common.java
@@ -1,6 +1,29 @@
 package pages;
 
+import utils.Driver;
+
 public class Common {
+
+    public static void setUpDriver() {
+        Driver.setDriver();
+    }
+
+    public static void openUrl(String url) {
+        Driver.getDriver().get(url);
+    }
+
+    public static void closeDriver() {
+        Driver.close();
+    }
+
+    public static void sleep(int millis) {
+
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
