4d393abb1145d62dc9e7db26d9d9eda86a5edf38
bmyko <b.mykolas@gmail.com>
2022-10-14 21:32:27 +0300

    Setting up TestListener class

--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -1,7 +1,15 @@
 package utils;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TestListener implements ITestListener {
 
@@ -11,6 +19,24 @@ public class TestListener implements ITestListener {
     }
 
     private void takeScreenshot() {
+        TakesScreenshot takesScreenshot = (TakesScreenshot) Driver.getDriver();
+        File screenshotRaw = takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+        String directory = "./screenshots";
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH_mm_ss_SSS");
+
+        String dateTime = localDateTime.format(formatter);
+
+        File screenshotFile = new File(directory + "/screenshot_" + dateTime + "png");
+
+        try {
+            FileUtils.copyFile(screenshotRaw,screenshotFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
