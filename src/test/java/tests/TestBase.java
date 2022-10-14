27e6c08450ae4b9a9c8c78bb728d99c631ac05e2
bmyko <b.mykolas@gmail.com>
2022-10-14 09:52:26 +0300

    Added Before/After methods to TestBase class

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,16 @@
 package tests;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.Common;
+
 public class TestBase {
-
-
+    @BeforeMethod
+    public void setUp() {
+        Common.setUpDriver();
+    }
+    @AfterMethod
+    public void tearDown() {
+        Common.closeDriver();
+    }
 }
